c021c14d2012541d3abca915b47004edc24ec292
SiekeMIS <lucasrosas.991402@gmail.com>
2025-06-25 16:56:45 -0400

    Tu mensaje de commit aquí

---
+++ app/Http/Controllers/Api/libroController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class libroController extends Controller
+{
+    public function index()
+    {
+        return 'obteniendo lista de estudiantes desde el controlador';
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use app\Http\Controllers\Api\libroController;
+
+Route::get('/libros', [libroController::class, 'index']);
+
+Route::get('/libros/{id}', function (){
+    return 'Obteniendo Un Libro';
+});
+
+Route::post('/libros', function (){
+    return 'Creando libros';
+});
+
+Route::put('/libros/{id}', function (){
+    return 'Actualizando Libros';
+});
+
+Route::delete('/libros/{id}', function (){
+    return 'Eliminando Libros';
+});
